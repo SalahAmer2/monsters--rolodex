02383352487ffa527ef0a5528b0eb588ebc0b2bd
Salah Amer <Salah@example.com>
2019-12-21 11:48:45 +0200

    Updates

---
+++ precache-manifest.8fa34a924d6869234e48ee6f17572f7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47bf493f08b2a322904f9e4748ce67c1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a7834732cbf595e87bcb",
+    "url": "/monsters-rolodex/static/css/main.7aa0a2cd.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a7834732cbf595e87bcb",
+    "url": "/monsters-rolodex/static/js/main.9856624e.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
